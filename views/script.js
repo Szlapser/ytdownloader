53575b20ce43711e3a40314c69cb081b8a441883
Szlapser <127340391+Szlapser@users.noreply.github.com>
2023-03-10 22:29:22 +0100

    Update script.js

--- views/script.js
+++ views/script.js
@@ -4,7 +4,7 @@ const urlInput = document.getElementById('url-input')
 
 convertBtn.addEventListener('click', () => {
     if (select.innerHTML == '') {
-        window.location.href = 'https://ytdownloader-uk3v.onrender.com//?url='+urlInput.value
+        window.location.href = 'https://ytdownloader-uk3v.onrender.com/?url='+urlInput.value
     }else {
         if (select.value!='') {
             window.location.href = 'https://ytdownloader-uk3v.onrender.com/download?itag='+select.value+'&url='+window.location.href.split('?url=')[1]
