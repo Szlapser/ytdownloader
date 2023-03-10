809ab5c5ba7ec9280008ab7e6c22d489b7e75c14
Szlapser <127340391+Szlapser@users.noreply.github.com>
2023-03-10 22:23:05 +0100

    Update script.js

--- views/script.js
+++ views/script.js
@@ -4,10 +4,10 @@ const urlInput = document.getElementById('url-input')
 
 convertBtn.addEventListener('click', () => {
     if (select.innerHTML == '') {
-        window.location.href = 'https://burgundy-beaver-sari.cyclic.app/?url='+urlInput.value
+        window.location.href = 'https://ytdownloader-uk3v.onrender.com//?url='+urlInput.value
     }else {
         if (select.value!='') {
-            window.location.href = 'https://burgundy-beaver-sari.cyclic.app/download?itag='+select.value+'&url='+window.location.href.split('?url=')[1]
+            window.location.href = 'https://ytdownloader-uk3v.onrender.com//download?itag='+select.value+'&url='+window.location.href.split('?url=')[1]
 
         
     }
