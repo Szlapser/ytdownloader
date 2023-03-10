6b22da6bd1f76c43d65aa0807ebc69ef406bcf7b
Szlapser <127340391+Szlapser@users.noreply.github.com>
2023-03-10 22:05:04 +0100

    Update script.js

--- views/script.js
+++ views/script.js
@@ -4,10 +4,10 @@ const urlInput = document.getElementById('url-input')
 
 convertBtn.addEventListener('click', () => {
     if (select.innerHTML == '') {
-        window.location.href = 'http://localhost:3000/?url='+urlInput.value
+        window.location.href = 'https://burgundy-beaver-sari.cyclic.app/?url='+urlInput.value
     }else {
         if (select.value!='') {
-            window.location.href = 'http://localhost:3000/download?itag='+select.value+'&url='+window.location.href.split('?url=')[1]
+            window.location.href = 'https://burgundy-beaver-sari.cyclic.app/download?itag='+select.value+'&url='+window.location.href.split('?url=')[1]
 
         
     }
